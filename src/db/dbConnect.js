df0b4cea292f8acc3d0121037629ddf7449ab6e6
Pratush <pratushbarua26@gmail.com>
2025-12-26 20:00:28 +0600

    added more packages

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -2,9 +2,7 @@ import mongoose from 'mongoose'
 
 export default async function ConnectDB() {
     try {
-        const dbConnection = await mongoose.connect(`${process.env.DB_URL}chai-backend`)
-        console.log(dbConnection)
-        return dbConnection;
+        await mongoose.connect(`${process.env.DB_URL}chai-backend`)
     } catch (error) {
         console.error("Error from DB : ", error);
     }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from "dotenv"
+import { app } from "./app.js";
+import ConnectDB from "./db/dbConnect.js";
+dotenv.config({ path: './.env' })
 
-dotenv.config({path: './env'})
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.get('/', () => {
-    res.send("op running")
-})
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT}`)
-})
+ConnectDB()
+    .then(() => app.listen(process.env.PORT || 8000, () => console.log(`⚙️ Server is running at port : ${process.env.PORT}`)))
+    .catch((err) => console.log("MONGO db connection failed !!! ", err))
